d9be9cf7c8be68650930a0ff30c63f8e64dfdb73
jrcastropy <jayrcastro.py@gmail.com>
2021-08-24 02:03:21 +0800

    change get to post

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from itertools import chain
 import json
@@ -9,7 +9,7 @@ from nltk.corpus import wordnet
 nltk.download('wordnet')
 
 application = Flask(__name__)
-api = Api(application)
+api         = Api(application)
 
 @application.route('/', methods=['GET', 'POST'])
 def hello_world():
@@ -17,16 +17,21 @@ def hello_world():
 
 class SynApi(Resource):
 
-    def get(self, word):
+    def post(self):
+        payl        = request.get_json()
+        data_list   = payl['data']
 
-        synonyms = wordnet.synsets(word)
-        lemmas = list(chain.from_iterable([word.lemma_names() for word in synonyms]))
+        res_dict    = {}
 
-        x = json.dumps(lemmas)
-        return {"synonyms":x}
+        for word in data_list:
+            synonyms        = wordnet.synsets(word)
+            lemmas          = list(dict.fromkeys(chain.from_iterable([word.lemma_names() for word in synonyms])))
+            result_synonyms = json.dumps(lemmas)
+            res_dict[word]  = result_synonyms
 
+        return jsonify(res_dict)
 
-api.add_resource(SynApi, '/Syn/<string:word>')
+api.add_resource(SynApi, '/Syn')
 
 if __name__ == '__main__':
     application.run(debug=True)
